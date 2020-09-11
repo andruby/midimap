3203eca9efd353bb605e4c161c3ae7c126a5eb83
Andrew Fecheyr <andrew@bedesign.be>
2020-09-11 20:29:14 +0200

    Debounce volume setting

--- listen.rb
+++ listen.rb
@@ -3,11 +3,10 @@ require "debouncer"
 
 input = UniMIDI::Input.gets
 
-def set_brightness(value)
-  @brightness_debouncer ||= Debouncer.new(0.1) do |val|
-    spawn("./ddcctl -d 1 -b #{val}")
-  end
-  @brightness_debouncer.call(value)
+def debouncer(uid, delay, *args, &block)
+  @debouncers ||= {}
+  @debouncers[uid] ||= Debouncer.new(delay, &block)
+  @debouncers[uid].call(*args)
 end
 
 MIDI.using(input) do
@@ -16,11 +15,15 @@ MIDI.using(input) do
     puts message.inspect
 
     if(message.channel == 0 && message.index == 14)
-      set_brightness(message.value)
+      debouncer(:brightness, 0.1, message.value) do |val|
+        spawn("./ddcctl -d 1 -b #{val}")
+      end
     end
 
-    if(message.channel == 0 && message.index == 15)
-      spawn("osascript -e 'set volume output volume #{message.value}'")
+    if(message.channel == 0 && message.index == 2)
+      debouncer(:volume, 0.05, message.value*(100.0/127.0)) do |val|
+        spawn("osascript -e 'set volume output volume #{val}'")
+      end
     end
 
   end
